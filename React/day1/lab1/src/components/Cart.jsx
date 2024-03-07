68e62fdd5a38035bb43bf1ab2a61a2c77e6ca8f6
ahmedgebreeel <ahmedgebreeeel@gmail.com>
2024-03-07 14:19:14 +0200

    handel reset and increment and decrement in day1 react

--- React/day1/lab1/src/components/Cart.jsx
+++ React/day1/lab1/src/components/Cart.jsx
@@ -8,10 +8,54 @@ const Cart = () => {
       { name: "Fries", counter: 0 },
       { name: "Water", counter: 0 },
     ]);
+
+    const Increment = (product)=>{
+      //clone 
+      const newProducts = [...products];
+      const index = newProducts.findIndex((p)=> p.name === product.name) //// get the index 
+      newProducts[index] = {...newProducts[index]}   ////deep clone
+      //edit
+      newProducts[index].counter += 1 ; 
+      //set state 
+      setProducts(newProducts);
+
+      
+  }
+
+  const Decrement = (product)=>{
+  
+    // // clone 
+    // let newProducts = [...products];
+    
+    //edit
+    let newProducts = products.map((p)=>{
+     if (p.name === product.name){
+       p = {...p, counter: p.counter -= 1};
+     }
+     return p;
+
+   });
+     
+    //set state 
+    setProducts(newProducts);
+    
+  }; 
+
+    const Reset = ()=>{
+      //clone 
+      let newProducts = [...products];
+      //edit
+      newProducts = newProducts.map(product => {return {...product, counter: 0}});
+      //setState
+      setProducts(newProducts);
+      
+    };
   return (
     <div>
-      {products.map(product => <CartItem key={product.name}/>)}
+      {products.map(product => <CartItem key={product.name} product = {product} Increment={Increment} Decrement={Decrement}/>)}
+      <button onClick={Reset} className="border border-blue-500 m-5 p-3">Reset</button>
     </div>
+
   );
 };
 
--- React/day1/lab1/src/components/CartItem.jsx
+++ React/day1/lab1/src/components/CartItem.jsx
@@ -1,32 +1,15 @@
 import { useState } from "react";
 
-const CartItem = ()=>{
-    const [data, setData] = useState({name: "Burger", counter: 0});
+const CartItem = ({product, Increment, Decrement})=>{
+    
+   
 
-    const Increment = ()=>{
-        // it is an obj so it 3 steps 
-        // 1.cloning
-        const newData = {...data};
-        //2. edit
-        newData.counter += 1;
-        //3. setState
-        setData(newData);
-    }
-
-    const Decrement = ()=>{
-        //cloning
-        const newData = {...data};
-        // edit
-        newData.counter -= 1;
-        //setState
-        setData(newData);
-    };
     return (
         <div className="border m-5 p-2">
-            <span className="border border-red-500 red p-2 m-3">{data.name}</span>
-            <span className="border border-b-black m-5 p-3">{data.counter}</span>
-            <button onClick={Increment} className="border border-blue-500 m-5 p-3">+</button>
-            <button onClick={Decrement} className="border border-blue-500 m-5 p-3">-</button>
+            <span className="border border-red-500 red p-2 m-3">{product.name}</span>
+            <span className="border border-b-black m-5 p-3">{product.counter}</span>
+            <button onClick={()=>Increment(product)} className="border border-blue-500 m-5 p-3">+</button>
+            <button onClick={()=>Decrement(product)} className="border border-blue-500 m-5 p-3">-</button>
 
         </div>
     )
